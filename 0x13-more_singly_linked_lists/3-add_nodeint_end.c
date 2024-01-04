2fb7fdd0f4707bf2f58b6c98760b49482765ced2
Wakiuru-Mathenge <wakymathenge@gmail.com>
2024-01-04 04:05:48 -0800

    3-add_nodeint_end.c

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
- * add_nodeint_end_ - adds a node at the end of a linked list
+ * add_nodeint_end - adds a node at the end of a linked list
  * @head: pointer to the first element in the list
  * @n: data to insert in the new element
  *
